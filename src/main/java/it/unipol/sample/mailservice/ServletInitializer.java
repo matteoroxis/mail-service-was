e0c9528a35a6a5a6c0a7466cb78f37702e1e7a41
matteo <matt.rossi@reply.it>
2018-11-09 16:38:06 +0100

    Add JMS Message and LDAP Bind

--- src/main/java/it/unipol/sample/mailservice/ServletInitializer.java
+++ src/main/java/it/unipol/sample/mailservice/ServletInitializer.java
@@ -3,14 +3,25 @@ package it.unipol.sample.mailservice;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.web.WebApplicationInitializer;
 
-
+@EnableJms
 @SpringBootApplication
-public class ServletInitializer extends SpringBootServletInitializer{
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ServletInitializer.class);
+    }
+
+    @Bean
+    public StrictHttpFirewall httpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        return firewall;
     }
 
 }
--- src/main/java/it/unipol/sample/mailservice/SpringSecurityConfig.java
+++ src/main/java/it/unipol/sample/mailservice/SpringSecurityConfig.java
@@ -6,6 +6,7 @@
 package it.unipol.sample.mailservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,15 +25,44 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationEntryPoint authenticationEntryPoint;
 
+    @Value("${spring.ldap.urls}")
+    private String ldapUrls;
+    @Value("${spring.ldap.base.dn}")
+    private String ldapBaseDn;
+    @Value("${spring.ldap.username}")
+    private String ldapSecurityPrincipal;
+    @Value("${spring.ldap.password}")
+    private String ldapPrincipalPassword;
+    @Value("${spring.ldap.user.dn.pattern}")
+    private String ldapUserDnPattern;
+    @Value("${spring.ldap.enabled}")
+    private String ldapEnabled;
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic()
-                .authenticationEntryPoint(authenticationEntryPoint);
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and().csrf().disable().httpBasic();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}reply.01!").roles("USER");
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        if (Boolean.parseBoolean(ldapEnabled)) {
+            auth
+                    .ldapAuthentication()
+                    .contextSource()
+                    .url(ldapUrls + ldapBaseDn)
+                    .managerDn(ldapSecurityPrincipal)
+                    .managerPassword(ldapPrincipalPassword)
+                    .and()
+                    .userDnPatterns(ldapUserDnPattern);
+        } else {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user").password("{noop}reply.01!").roles("USER");
+        }
     }
 
 }
